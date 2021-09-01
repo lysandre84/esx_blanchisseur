f0e595ef92b5bc66cb518482c40de4b0aecf3f03
lysandre84 <89921892+lysandre84@users.noreply.github.com>
2021-09-01 15:44:29 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 Config					= {}
 Config.Locale			= 'fr'
 Config.Slice			= 1000
-Config.Percentage		= 50
+Config.Percentage		= 0
 Config.MarkerSize   	= {x = 3.0, y = 3.0, z = 3.0}
 Config.openHours		= 0
 Config.closeHours		= 5
-Config.Blip 			= true
-Config.Hours 			= true
+Config.Blip 			= false
+Config.Hours 			= false
 Config.Menu				= false
-Config.Bonus			= {min = 0, max = 05}
+Config.Bonus			= {min = 0, max = 0}
 
 Config.Zones = {
 	{x = -1117.0729980469, y = -503.17425537109, z = 34.806301116943,percent=30},
